29ee3350cc52814f32f68d2e4350155c22591a09
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-05-11 22:21:07 +0200

    project initialization

---
+++ src/Dependencies.Exchange.Base/IExchangeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Dependencies.Exchange.Base
+{
+    public interface IExchangeViewModel<T>
+    {
+        bool CanLoad { get; }
+
+        Func<Func<Task>, Task> RunAsync { get; set; }
+        string Title { get; }
+
+        Task<T> LoadAsync();
+    }
+
+}
---
+++ src/Dependencies.Exchange.Base/IImportAssembly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Dependencies.Exchange.Base.Models;
+
+namespace Dependencies.Exchange.Base
+{
+    public interface IImportAssembly : IExchangeSevice
+    {
+        Task<AssemblyExchangeContent> ImportAsync(Func<UserControl, IExchangeViewModel<AssemblyExchangeContent>, Task<AssemblyExchangeContent>> showDialog);
+    }
+}
